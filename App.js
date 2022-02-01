147c8bfcff67492e042efb45b3d1a70b9534edad
Piazzi <40416044+Piazzi@users.noreply.github.com>
2022-02-01 10:24:19 -0300

    Add api/drive keys

--- App.js
+++ App.js
@@ -2,6 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const driveId = "1W8wD_e3knzbh0IdhkPRmEFZ0_cxV4vvR";
+const apiKey = "AIzaSyAMOp34R_D_PTAiwrBY_xwjOnMzImPAbdA";
+
+const getFilesFromDrive = () => {
+  return fetch('https://www.googleapis.com/drive/v3/drives/'+ driveId)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      return json;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+console.log(getFilesFromDrive());
+
 export default function App() {
   return (
     <View style={styles.container}>
